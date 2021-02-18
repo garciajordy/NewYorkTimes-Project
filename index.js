99f97072beda1ccd0653137f69a2718b5179e4db
garciajordy <jordygarcia0119@gmail.com>
2021-02-18 18:46:52 +0200

    added javascript to my footer

--- index.js
+++ index.js
@@ -13,3 +13,30 @@ function openForm() {
     background.style.visibility = "visible"
 
 }
+let sum = 2
+var foot = document.getElementById("footy")
+let footertxt = document.getElementById("footerTxt")
+let expand = document.getElementById("expand")
+let arrow = document.getElementById("expandArrow")
+function footerOpen() {
+
+
+
+    if (sum % 2 == 0) {
+
+        foot.style.marginBottom = "0"
+        sum++
+        expand.innerHTML = "collapse"
+        footertxt.innerHTML = "Thanks for reading The Times"
+        footertxt.style.fontWeight = "900"
+    }
+    else {
+        foot.style.marginBottom = "-200px "
+        sum++
+        footertxt.style.fontWeight = "100"
+        expand.innerHTML = "expand"
+        footertxt.innerHTML = "Access more of The Times by creating a free account or logging in."
+
+    }
+
+}
